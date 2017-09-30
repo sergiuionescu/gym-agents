a63ea9b536eee866b7216f58ed8b3804300c9443
Sergiu Ionescu <sergiu.ionescu@gmail.com>
2017-09-30 13:04:41 +0300

    Extracted world module Adding Grafana monitoring Cli parameters

--- run.py
+++ run.py
@@ -4,11 +4,21 @@ import numpy as np
 import json
 import os
 import logging
-import random
-import cPickle as pickle
-from agents import SpeculativeDiffAgent as Agent
+import statsd
+import argparse
 
-environment = 'Copy-v0'
+from worlds import World
+
+
+parser = argparse.ArgumentParser(description="Launches worlds")
+parser.add_argument('environment', nargs="?", default="Copy-v0")
+parser.add_argument('world', nargs="?", default="")
+args = parser.parse_args()
+
+statsd = statsd.StatsClient('localhost', 8125, prefix='agents')
+
+environment = args.environment
+world_name = args.world
 env = gym.make(environment)
 
 max_agents = 10
@@ -17,46 +27,20 @@ max_attempts = 100
 
 gym.scoreboard.api_key = "sk_MXIkB1v6Shebbl5pMWtTA"
 outdir = '/tmp/cem-agent-results'
+
 env = wrappers.Monitor(env, outdir, force=True)
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 np.random.seed(0)
 
-
-class World(object):
-    def __init__(self, environment):
-        self.population = {}
-        self.environment = environment
-
-        with open(os.path.join('data', 'worlds.txt'), 'r') as f:
-            data = f.readline()
-            names = data.split(',')
-            self.name = random.choice(names)
-
-        with open(os.path.join('data', 'people.txt')) as f:
-            self.names = map(lambda s: s.strip(), f.readlines())
-
-    def birth(self, agent):
-        agent.name = random.choice(self.names)
-        self.population[self.population.__len__()] = agent
-
-    def sleep(self):
-        path = os.path.join('rem', self.environment, self.name)
-        if not os.path.exists(path):
-            os.makedirs(path)
-
-        for key, agent in self.population.items():
-            pickle.dump(agent, open(os.path.join(path, str(agent.total_reward) + '.' + agent.name + '.pcl'), 'w'))
-
-
 world = World(environment)
+world.wake(world_name)
 
 found = False
-for max_agents in range(max_agents):
+for agent_position in range(max_agents):
     observation = env.reset()
-    agent = Agent(env.action_space)
-    world.birth(agent)
+    agent = world.get_agent(agent_position, env.action_space)
     for episodes in range(max_episodes):
         for t in range(max_attempts):
             action = agent.act(observation)
@@ -67,6 +51,9 @@ for max_agents in range(max_agents):
                 break
             else:
                 agent.total_reward += 1
+
+            statsd.set(world.name + '.' + agent.name, agent.total_reward)
+
             if done:
                 found = True
                 print("Episode finished after {} timesteps".format(t + 1))
@@ -88,4 +75,4 @@ world.sleep()
 
 logger.info(
     "Successfully ran cross-entropy method. Now trying to upload results to the scoreboard. If it breaks, you can always just try re-uploading the same results.")
-gym.upload(outdir)
+#gym.upload(outdir)
---
+++ worlds/World.py
@@ -0,0 +1,52 @@
+import os
+import random
+import cPickle as pickle
+
+from agents import SpeculativeDiffAgent as Agent
+
+
+class World(object):
+    def __init__(self, environment):
+        self.population = {}
+        self.environment = environment
+
+        with open(os.path.join('data', 'worlds.txt'), 'r') as f:
+            data = f.readline()
+            names = data.split(',')
+            self.name = random.choice(names)
+
+        with open(os.path.join('data', 'people.txt')) as f:
+            self.names = map(lambda s: s.strip(), f.readlines())
+
+    def get_agent(self, agent_position, action_space):
+        next_agent = self.population[agent_position] if agent_position in self.population else self.birth(action_space)
+
+        return next_agent
+
+    def birth(self, action_space):
+        agent = Agent(action_space)
+        agent.name = random.choice(self.names)
+        self.population[self.population.__len__()] = agent
+
+        return agent
+
+    def wake(self, name):
+        if name:
+            self.name = name
+            path = self.get_path()
+            if os.path.exists(path):
+                agent_paths = os.listdir(path)
+                agent_paths.sort(reverse=True)
+                for agent_path in agent_paths:
+                    self.population[self.population.__len__()] = pickle.load(open(os.path.join(path, agent_path), 'r'))
+
+    def sleep(self):
+        path = self.get_path()
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        for key, agent in self.population.items():
+            pickle.dump(agent, open(os.path.join(path, str(agent.total_reward) + '.' + agent.name + '.pcl'), 'w'))
+
+    def get_path(self):
+        return os.path.join('rem', self.environment, self.name)
